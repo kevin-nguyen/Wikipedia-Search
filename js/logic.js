631e2414c219f7714d3a0a1186b223281de4b46a
Kevin Nguyen <kevin.nguyen@engineering.ucla.edu>
2016-05-13 17:27:27 -0700

    Apply initial webpage design

---
+++ js/logic.js
@@ -0,0 +1,7 @@
+(function($) {
+    $(function() {
+        var $searchContainer = $("#search-container");
+        $searchContainer.addClass('hide-content');
+        $searchContainer.animate({opacity: 1}, 1500);
+    });
+})(window.jQuery);
