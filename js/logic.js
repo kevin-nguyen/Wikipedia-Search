da79b8f6860af143e7310844ec60e9b289abdbaa
Kevin Nguyen <kevin.nguyen@engineering.ucla.edu>
2016-05-17 01:02:21 -0700

    Add search functionality

--- js/logic.js
+++ js/logic.js
@@ -1,7 +1,49 @@
 (function($) {
     $(function() {
+        var domain = "https://en.wikipedia.org";
+        var queryString = "/w/api.php?action=query&format=json&prop=extracts%7Cinfo&continue=&generator=search&redirects=1&exsentences=2&exlimit=20&exintro=1&explaintext=1&inprop=url&gsrnamespace=0&gsrlimit=20&gsrsearch=";
+        var $inputDOM = $('input');
+
         var $searchContainer = $("#search-container");
         $searchContainer.addClass('hide-content');
         $searchContainer.animate({opacity: 1}, 1500);
+
+        function fetchArticles(event, searchString) {
+            $.ajax({
+                url: domain + queryString + searchString,
+                cache: false,
+                dataType: 'jsonp',
+                type: 'GET',
+                success: function(response) {
+                    var articles = response.query.pages;
+
+                    $('#results-container').html("");
+
+                    for(var article in articles) {
+                        $('#results-container').append('<div class="row"><a href=\"' + articles[article].fullurl + '\" target="_blank"><div class="col-md-8 col-md-offset-2 results-styling">' 
+                                + '<h2>' + articles[article].title + '</h2>' 
+                                + '<br />' 
+                                + '<p>' + articles[article].extract + '</p>'
+                                + '</div></a></div>');
+                        
+                    }
+                }
+            });
+        }
+
+        $inputDOM.on('keyup', function(event) {
+            //event.preventDefault();
+
+            var keycode = event.keyCode;
+            //console.log(event.type + ': ' + keycode);
+
+            if (keycode === 13) {
+                fetchArticles(event, $inputDOM.val());
+            }
+        });
+
+        $("#search-button").on('click', function(event) {
+            fetchArticles(event, $inputDOM.val());
+        });
     });
 })(window.jQuery);
